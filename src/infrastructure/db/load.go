f4ef53304f8cccdd2d82c7575ae1a792d07d23d3
oskalo <oleg.skalozub1990@gmail.com>
2019-10-10 14:27:36 +0300

    add some refactoring for config, DB section

--- src/infrastructure/db/load.go
+++ src/infrastructure/db/load.go
@@ -14,13 +14,19 @@ var SQLBD SQLBDer
 
 // Load ...
 func Load() error {
-
 	conf := fmt.Sprintf("sslmode=disable host=%s port=%d user=%s dbname=%s password=%s",
-		config.Config.DBHost, config.Config.DBPort, config.Config.DBUser, config.Config.DBName, config.Config.DBPassword)
+		config.Config.DBConfig.DBHost, config.Config.DBConfig.DBPort, config.Config.DBConfig.DBUser, config.Config.DBConfig.DBName, config.Config.DBConfig.DBPassword)
 
-	db, err := gorm.Open(config.Config.DBDialect, conf)
+	db, err := gorm.Open(config.Config.DBConfig.DBDialect, conf)
+	if err != nil {
+		return nil
+	}
+
+	db.DB().SetMaxOpenConns(config.Config.DBConfig.DBConn)
+	db.DB().SetMaxIdleConns(config.Config.DBConfig.DBIdleConn)
 
 	SQLBD = sqlDB{db: db}
+
 	return err
 }
 
